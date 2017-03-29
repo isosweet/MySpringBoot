990a08856e54946124a311f9ee59d912fc4beefd
isosweet <405438526@qq.com>
2017-03-29 09:44:23 +0800

    MySpringBoot

---
+++ MySpringBoot-Actuator/src/main/java/com/mySpringBoot/actuator/MySpringBootActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.mySpringBoot.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootActuatorApplication.class, args);
+	}
+}
